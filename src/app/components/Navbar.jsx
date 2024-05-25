9a195bdc1232b9f6e20bc10ff720855e23d9dd7f
hermani <hermani457@gmail.com>
2024-05-25 16:48:59 -0400

    chore: Update Navbar component to include logo and improve spacing
    The Navbar component is modified to include the logo and improve spacing by adding a margin-bottom. This enhances the visual appearance and user experience of the application.

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -36,15 +36,9 @@ const Navbar = () => {
   return (
     <nav className="bg-text">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-5">
-        {/* <a href="/" className=""> */}
-        {/* <img src={logo} className="h-10 w-20" alt="logo yme" /> */}
-        <div>
+        <div className="">
           <Image src={logo} alt="logo" width={125} className="m-0 p-0" />
         </div>
-        {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Zona Vip
-          </span> */}
-        {/* </a> */}
         <button
           type="button"
           onClick={toggleMenu}
@@ -74,24 +68,36 @@ const Navbar = () => {
           }`}
           id="navbar-default"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:border-0">
             <li>
-              <Link className="block py-2 px-3 text-white rounded hover:bg-gray-500 md:hover:bg-transparent md:hover:text-primary md:border-0 md:p-0 " href={"#home"}>
+              <Link
+                className="block py-2 px-3 text-white rounded hover:bg-gray-500 md:hover:bg-transparent md:hover:text-primary md:border-0 md:p-0 "
+                href={"#home"}
+              >
                 Home
               </Link>
             </li>
             <li>
-              <Link className="block py-2 px-3 text-white rounded hover:bg-gray-500 md:hover:bg-transparent md:hover:text-primary md:border-0 md:p-0 " href={"#mision"}>
+              <Link
+                className="block py-2 px-3 text-white rounded hover:bg-gray-500 md:hover:bg-transparent md:hover:text-primary md:border-0 md:p-0 "
+                href={"#mision"}
+              >
                 Mision y Vision
               </Link>
             </li>
             <li>
-              <Link className="block py-2 px-3 text-white rounded hover:bg-gray-500 md:hover:bg-transparent md:hover:text-primary md:border-0 md:p-0 " href={"#expertise"}>
+              <Link
+                className="block py-2 px-3 text-white rounded hover:bg-gray-500 md:hover:bg-transparent md:hover:text-primary md:border-0 md:p-0 "
+                href={"#expertise"}
+              >
                 Expertise
               </Link>
             </li>
             <li>
-              <Link className="block py-2 px-3 text-white rounded hover:bg-gray-500 md:hover:bg-transparent md:hover:text-primary md:border-0 md:p-0 " href={"#contact"}>
+              <Link
+                className="block py-2 px-3 text-white rounded hover:bg-gray-500 md:hover:bg-transparent md:hover:text-primary md:border-0 md:p-0 "
+                href={"#contact"}
+              >
                 Contacto
               </Link>
             </li>
