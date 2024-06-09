fd438ba28d656e6718679584f45e0dc7a8474794
hermani <hermani457@gmail.com>
2024-06-08 23:00:39 -0400

    refactor: Remove unused image files and update Photogrid component
    The unused image files "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", and "6.jpg" were deleted from the "public/assets/img/land" directory. Additionally, the Photogrid component was updated to include new image files "7.jpg", "8.jpg", and "9.jpg". This change improves the efficiency and organization of the image assets in the application.

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import happy from "../../../public/assets/img/happy.webp";
 import Image from "next/image";
-import PhotoGrind from "./PhotoGrind";
+import Photogrind from "./Photogrid";
 
 const Hero = () => {
   return (
     <section id="home" className="bg-background">
       <div className="max-w-screen-xl mx-auto min-h-[90vh] p-5 my-auto flex items-center">
-        <div className="flex justify-center items-center h-full gap-5 flex-wrap flex-col md:flex-row">
-          <div className="flex-1 mx-auto space-y-5">
+        <div className="flex justify-center items-center h-full gap-5 flex-wrap flex-col lg:flex-row">
+          <div className="flex-1 space-y-5">
             <h1 className="text-5xl md:text-8xl bg-gradient-to-r font-medium from-accent via-primary to-secondary font-bebasNeue inline text-transparent bg-clip-text">
               YMe Recruiment
             </h1>
@@ -26,8 +26,8 @@ const Hero = () => {
             </p>
           </div>
           <div className="flex flex-1 justify-center min-w-80 overflow-hidden">
-            <Image src={happy} alt="foto" className="rounded-md" />
-            {/* <PhotoGrind /> */}
+            {/* <Image src={happy} alt="foto" className="rounded-md" /> */}
+            <Photogrind />
           </div>
         </div>
       </div>
